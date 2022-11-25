c7017b6c315297670ba580f8a38156b03fbd8249
soomerss <poqw741@naver.com>
2022-11-25 09:04:48 +0900

    add : new File add

---
+++ Hello.java
@@ -0,0 +1,10 @@
+
+
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        MathGenerator mg = new MathGenerator();
+        int result = mg.add(3,4);
+        System.out.println(result);
+    }
+}
