bdf920d16c7a9dfbb07b27f590c30ca1723ee412
soomerss <poqw741@naver.com>
2022-11-29 12:22:00 +0900

    push전에 한번 해봤어요

--- Hello.java
+++ Hello.java
@@ -3,9 +3,8 @@
 public class Hello{
     public static void main(String[] args) {
         System.out.println("Hello World");
-        MathGenerator mg = new MathGenerator();
-        int result = mg.add(3,4);
-        System.out.println(result);
+
         System.out.println(4-3);
+        System.out.println("새로 추가했는데요?");
     }
 }
